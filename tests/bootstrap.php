6a8129d1e699e794554400cbaf81138abed2c2eb
blanchonvincent <blanchon.vincent@gmail.com>
2013-01-06 21:30:27 +0100

    Fix autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
-define('ZF2_PATH', realpath(__DIR__ . '/../../zf2-fork/library/'));
+defined('ZF2_PATH') || define('ZF2_PATH', realpath(__DIR__ . '/../../zf2-fork/library/'));
 include __DIR__ . '/_autoload.php';
