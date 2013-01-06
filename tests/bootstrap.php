e78dcdc0225d2a3765fd0ad704a7b124a9bd8dc4
blanchonvincent <blanchon.vincent@gmail.com>
2013-01-06 21:25:32 +0100

    Fix autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,4 @@
 <?php
 
-if(!defined('ZF2_PATH')) {
-    define('ZF2_PATH', realpath(__DIR__ . '/../../zf2-fork/library/'));
-}
-
+define('ZF2_PATH', realpath(__DIR__ . '/../../zf2-fork/library/'));
 include __DIR__ . '/_autoload.php';
--- tests/bootstrap.travis.php
+++ tests/bootstrap.travis.php
@@ -1,7 +1,4 @@
 <?php
 
-if(!defined('ZF2_PATH')) {
-    define('ZF2_PATH', realpath(__DIR__ . '/../zendframework/zendframework/library/'));
-}
-
+define('ZF2_PATH', realpath(__DIR__ . '/../zendframework/zendframework/library/'));
 include __DIR__ . '/_autoload.php';
