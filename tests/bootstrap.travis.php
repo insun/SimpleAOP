e44524ec8518b8641c3bfec95730366db0d4672e
blanchonvincent <blanchon.vincent@gmail.com>
2013-01-06 21:35:05 +0100

    Fix autoload for travis

--- tests/bootstrap.travis.php
+++ tests/bootstrap.travis.php
@@ -1,4 +1,4 @@
 <?php
 
-defined('ZF2_PATH') || define('ZF2_PATH', __DIR__ . '/../zendframework/zendframework/library');
+defined('ZF2_PATH') || define('ZF2_PATH', __DIR__ . '/../vendor/zendframework/zendframework/library');
 include __DIR__ . '/_autoload.php';
